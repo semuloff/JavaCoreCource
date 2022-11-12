28fa8ae5819e9dfa1ee1639b1dbc432d868bcd5f
SEMUL <semul.main@icloud.com>
2022-11-12 23:20:11 +0300

    Objects and minor OOP

---
+++ src/ObjectsStaticMethodsFields/Main.java
@@ -0,0 +1,17 @@
+package ObjectsStaticMethodsFields;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Person.count + "\n");
+        Person Racer = new Person();
+        System.out.println(Person.count + "\n");
+        Person Manager = new Person("Joel", 32, 176.4f);
+        System.out.println(Person.count + "\n");
+
+        Racer.printItems();
+        Racer.setItems("Martin", 6, 144.f);
+        Racer.printItems();
+
+        Manager.printItems();
+    }
+}
---
+++ src/ObjectsStaticMethodsFields/Person.java
@@ -0,0 +1,44 @@
+package ObjectsStaticMethodsFields;
+
+public class Person {
+    public static int count = 0;
+    private String name;
+    private int age;
+    private float growth;
+
+    public Person() {
+        increment();
+        setItems("None", 0, 0f);
+    }
+
+    public Person(String name, int age, float growth) {
+        increment();
+        setItems(name, age, growth);
+    }
+
+    private void increment() {
+        count++;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGrowth(float growth) {
+        this.growth = growth;
+    }
+
+    public void setItems(String name, int age, float growth) {
+        setName(name);
+        setAge(age);
+        setGrowth(growth);
+    }
+
+    public void printItems() {
+        System.out.println("Name: " + this.name + "\nAge: " + this.age + "\nGrowth: " + growth + "\n");
+    }
+}
