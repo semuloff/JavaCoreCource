c5900d0671bfe0058fe1dd3fe2b9ee8d58e341aa
SEMUL <semul.main@icloud.com>
2022-11-13 22:08:39 +0300

    conditions and loops

---
+++ src/conditionsAndLoops.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class conditionsAndLoops {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your hidden number: ");
+        int hidNumber = Integer.parseInt(reader.readLine());
+
+        int quess;
+
+        while (true) {
+            System.out.print("\nEnter your quess number: ");
+            quess = Integer.parseInt(reader.readLine());
+
+            if (quess == hidNumber) {
+                System.out.print("\nFine! You quessed!");
+                break;
+            }
+
+            System.out.print("\nMiss:) ");
+        }
+    }
+}
